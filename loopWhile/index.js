61970ec3459abb72c572c133766f2cf2778a9d5c
tskkks <130366264+tskkks@users.noreply.github.com>
2024-09-26 08:13:24 -0300

    loopWhile

---
+++ loopWhile/index.js
@@ -0,0 +1,23 @@
+let n = 1
+
+/*while(true) {
+let n2 = (Math.random() * 60).toFixed(2)
+console.log(n2)
+}*/
+
+
+
+
+// while (n <=10) {
+//               //CTRL+C PARA PARAR O LOOP
+//   console.log(n)
+//   n++
+// }
+
+// WHILE VS DO WHILE
+do {
+  console.log("----MENU----");
+  console.log("Qual é o seu nome?");
+  console.log("------------------");
+} while(n < 10)
+
