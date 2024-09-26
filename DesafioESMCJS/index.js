4e9435b3adebe0f2ff5f931c77c546584bdbb67f
tskkks <130366264+tskkks@users.noreply.github.com>
2024-09-26 08:20:17 -0300

    Desafio_ESM_CJS

---
+++ DesafioESMCJS/index.js
@@ -0,0 +1,10 @@
+// 1) Liberar "type":"module"
+// 2) Criar um módulo veiculo.js
+//      // acelerar - return "x" kms/h
+//      // mostrarDadosVeiculo (marca, modelo, fabricante)
+// 3) Criar módulo CJS - require('fs')
+//     // console.log(fs) - * criar um arquivo
+
+// 4) Criar um módulos ESM - index.js
+//    // importar e usar todos os módulos exportados
+//    // anteriormente
