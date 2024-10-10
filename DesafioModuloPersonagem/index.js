efb0e5cad32ddef6bda11fe3311943c3ba925126
tskkks <130366264+tskkks@users.noreply.github.com>
2024-10-10 08:23:19 -0300

    DesafioModuloPersonagem

---
+++ DesafioModuloPersonagem/index.js
@@ -0,0 +1,5 @@
+//crie um modulo ESM personagem.js
+//crie funções para:
+//imprimir um obnjeto com (nome, poder, vida)
+//exporte a função (imprimirPersonagem)
+//importe o modulo dentro do seu arquivo index.js
